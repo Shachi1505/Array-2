febeac52b9c49cdafd4daa1292151db4ce1b3b42
Shachi Singhal <122254334+Shachi1505@users.noreply.github.com>
2023-07-30 12:22:34 +0530

    Create Q3.java
    To find the kth smallest element in an array after sorting it.

---
+++ Q3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        Arrays.sort(arr);
+        System.out.println(arr[k - 1]);
+    }
+}
